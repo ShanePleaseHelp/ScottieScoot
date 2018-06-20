2770d92bd1e10a57d6e038d79897061100233c26
Shane <shane.d@live.com>
2018-06-20 13:31:28 -0400

    Changed sysem commands

--- scottie01.rb
+++ scottie01.rb
@@ -3,28 +3,31 @@ require 'fileutils'
 
 puts "###Scottie Creator has Started###"
 puts
-puts "Updating System"
+puts "Updating System....................."
+puts
 
-system sudo apt-get update
+system("sudo apt-get -v update")
 
 puts
-puts "Installing fbi"
-
-system sudo apt-get install fbi
+puts "Installing fbi........................"
+puts
+system("sudo apt-get install fbi")
 
 puts
-Puts "Installing Remote.it"
-
-system sudo apt-get install weavedconnectd
+puts "Installing Remote.it.........................."
+puts
+system("sudo apt-get install weavedconnectd")
 
 	Dir.mkdir 'scottie'
-	
-	FileUtils.copy(/slideshow.sh, /home/pi/scottie/slideshow.sh)
+
+system("mv -f -v slideshow.sh /home/pi/scottie")
+system("mv -f -v file.tmp /home/pi/scottie")
 	
 	Dir.chdir('/home/pi/scottie')
 
 puts
 puts "Make sure there are atleast 2 picture files in website folder"
+puts
 puts
 puts "What website folder to download pictuers from?"
 
@@ -37,10 +40,10 @@ art.close
 
 FileUtils.mv("file.tmp", "getart.sh")
 
-system sudo chmod +x getart.sh
-system sudo chmod +x slideshow.sh
+cmd = "chmod +x getart.sh"
+cmd = "chmod +x slideshow.sh"
 
-RC = File.open ("/home/pi/.bashrc",'a')
+RC = File.open (".bashrc")
 RC << bashrc.tmp
 RC.close
 
